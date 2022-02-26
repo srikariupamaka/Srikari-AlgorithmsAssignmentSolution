afe288f000f596b1d07ecf3508b9f49170f03524
srikari <srikariupamaka@gmail.com>
2022-02-26 21:05:48 +0530

    first commit

---
+++ src/com/greatlearning/Display/Display.java
@@ -0,0 +1,27 @@
+package com.greatlearning.Display;
+
+public class Display {
+	public void DisplayRise(double[] arr, boolean[] compare){
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            if (compare[i]){
+                count++;
+            }
+        }
+        System.out.println("Total no of companies whose stock price rose today : " + count);
+    }
+     public void DisplayDown(double[] arr, boolean[] compare){
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            if (compare[i]==false){
+
+                count++;
+            }
+        }
+         System.out.println("Total no of companies whose stock price declined today : " + count);
+    }  
+
+}
+
---
+++ src/com/greatlerning/Data/BinarySearchImpl.java
@@ -0,0 +1,25 @@
+package com.greatlerning.Data;
+
+public class BinarySearchImpl {
+	public void BinarySearch(double[] arr, double target) {
+	       int start = 0;
+	       int end = arr.length - 1;
+
+	       while(start<= end){
+	           int mid = end - (end - start)/2;
+
+	           if (arr[mid]== target){
+	               System.out.println("Stock of value " + target + " is present");
+	               return;
+	           }
+	           if (arr[mid] > target){
+	               end = mid -1;
+	           }
+
+	           if (arr[mid] < target){
+	               start = mid  + 1;
+	           }
+	       }
+	       System.out.println("Value not found");
+	   }
+}
